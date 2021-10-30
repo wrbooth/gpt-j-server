cb5e2a81141d0da8e5529f99206cf4e98f01180c
Bill Booth <wrbooth@gmail.com>
2021-10-30 03:35:48 +0000

    next

--- client.py
+++ client.py
@@ -1,5 +1,11 @@
 import curses
 import argparse
+import sys
+
+class Window:
+    def __init__(self, n_rows, n_cols):
+        self.n_rows = n_rows
+        self.n_cols = n_cols
 
 
 def main(stdscr):
@@ -10,14 +16,17 @@ def main(stdscr):
     with open(args.filename) as f:
         buffer = f.readlines()
 
+    window = Window(curses.LINES - 1, curses.COLS - 1)
+
     while True:
         stdscr.erase()
-        for row, line in enumerate(buffer):
-            stdscr.addstr(row, 0, line)
+        for row, line in enumerate(buffer[:window.n_rows]):
+            stdscr.addstr(row, 0, line[:window.n_cols])
 
         k = stdscr.getkey()
         if k == "q":
             sys.exit(0)
+
 
 if __name__ == "__main__":
     curses.wrapper(main)
