d4ea9a31e77193b5003747fe578af7438639df44
Bill Booth <wrbooth@gmail.com>
2021-10-30 03:34:01 +0000

    next

--- client.py
+++ client.py
@@ -1,8 +1,20 @@
 import curses
+import argparse
 
 
 def main(stdscr):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename")
+    args = parser.parse_args()
+
+    with open(args.filename) as f:
+        buffer = f.readlines()
+
     while True:
+        stdscr.erase()
+        for row, line in enumerate(buffer):
+            stdscr.addstr(row, 0, line)
+
         k = stdscr.getkey()
         if k == "q":
             sys.exit(0)
