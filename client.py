030d2397db58ff49623354bb499ef4f766091f90
Bill Booth <wrbooth@gmail.com>
2021-10-30 03:45:28 +0000

    more!!

--- client.py
+++ client.py
@@ -9,9 +9,19 @@ class Window:
 
 
 class Cursor:
-    def __init__(self, row=0, col=0):
+    def __init__(self, row=0, col=0, col_hint=None):
         self.row = row
-        self.col = col
+        self._col = col
+        self._col_hint = col if col_hint is None else col_hint
+
+    @property
+    def col(self):
+        return self._col
+
+    @col.setter
+    def col(self, col):
+        self._col = col
+        self._col_hint = col
 
     def up(self, buffer):
         if self.row > 0:
@@ -24,15 +34,22 @@ class Cursor:
             self._clamp_col(buffer)
 
     def _clamp_col(self, buffer):
-        self.col = min(self.col, len(buffer[self.row]))
+        self._col = min(self._col_hint, len(buffer[self.row]))
 
-    def left(self):
+    def left(self, buffer):
         if self.col > 0:
             self.col -= 1
+        elif self.row > 0:
+            self.row -= 1
+            self.col = len(buffer[self.row])
 
     def right(self, buffer):
         if self.col < len(buffer[self.row]):
             self.col += 1
+        elif self.row < len(buffer) - 1:
+            self.row += 1
+            self.col = 0
+
 
 def main(stdscr):
     parser = argparse.ArgumentParser()
@@ -59,7 +76,7 @@ def main(stdscr):
         elif k == "KEY_DOWN":
             cursor.down(buffer)
         elif k == "KEY_LEFT":
-            cursor.left()
+            cursor.left(buffer)
         elif k == "KEY_RIGHT":
             cursor.right(buffer)
 
